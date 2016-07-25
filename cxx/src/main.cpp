dd22df40a732b46365a58c14b58e1f204d762d5a
DengYiping <scottdeng@live.cn>
2016-07-25 19:00:17 +0800

    add two mode

--- cxx/src/main.cpp
+++ cxx/src/main.cpp
@@ -10,11 +10,22 @@
 #include "ahocorasick.hpp"
 #include "data_access.hpp"
 #include "aho_corasick.hpp"
+#include <set>
+#include "json.hpp"
 void large_memory_mode();
 void limited_memory_mode();
 
+void log(int id, int count){
+  using nlohmann::json;
+  json js;
+  js["id"] = id;
+  js["count"] = count;
+  std::cout<<js<<std::endl;
+}
+
 int main(int argc, const char * argv[]) {
   // insert code here...
+  limited_memory_mode();
   return 0;
 }
 
@@ -22,6 +33,10 @@ void large_memory_mode(){
   std::string filename = "dump.dat";
   auto signList = util::getAllSignatures(filename);
   std::vector<std::tuple<int, aho_corasick::trie*>> trees;
+  
+  
+  
+  //build all the trees
   for(auto sign:signList){
     int id = std::get<0>(sign);
     aho_corasick::trie* trie = new aho_corasick::trie();
@@ -30,17 +45,63 @@ void large_memory_mode(){
     }
     trees.push_back(std::make_tuple(id,trie));
   }
+  
+  
+  
+  
   std::string path;
   while(1){
     std::cin >> path;
     std::vector<std::string> files = util::getFilenameVector(path);
     for(std::string file: files){
       for(std::tuple<int,aho_corasick::trie*> tuple: trees){
+        std::set<std::string> match_set;
         int id = std::get<0>(tuple);
         std::string file_string = util::getHexString(file);
         aho_corasick::trie* tree = std::get<1>(tuple);
-        auto result = tree->parse_text(file_string);
+        auto results = tree->parse_text(file_string);
+        for(auto result: results){
+          if(!result.is_empty()){
+            match_set.insert(result.get_keyword());
+          }
+        }
+        int count = match_set.size();
+        log(id, count);
       }
     }
   }
+}
+
+
+
+
+void limited_memory_mode(){
+  std::string filename = "dump.dat";
+  auto signList = util::getAllSignatures(filename);
+  std::string path;
+  
+  while(1){
+    std::cin >> path;
+    std::vector<std::string> files = util::getFilenameVector(path);
+    for(auto sign:signList){
+      int id = std::get<0>(sign);
+      aho_corasick::trie trie;
+      for(auto entry: std::get<1>(sign)){
+        trie.insert(entry);
+      }
+      for(std::string file: files){
+        std::set<std::string> match_set;
+        std::string file_string = util::getHexString(file);
+        auto results = trie.parse_text(file_string);
+        for(auto& result: results){
+          if(!result.is_empty()){
+            match_set.insert(result.get_keyword());
+          }
+        }
+        int count = match_set.size();
+        log(id, count);
+      }
+    }
+  }
+  
 }
