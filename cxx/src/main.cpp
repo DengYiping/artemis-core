17dc9ac5d2709b1a37b6352f866671d21fae18ba
Deng Yiping <scottdeng@live.cn>
2016-07-26 19:50:58 +0800

    fix bug

--- cxx/src/main.cpp
+++ cxx/src/main.cpp
@@ -19,162 +19,7 @@ void limited_memory_mode();
 
 
 
-
-
-
-
-
-
-
-
-
-
-namespace util_{
-#include <iostream>
-#include <map>
-#include <vector>
-#include <string>
-#include <queue>
-
-using std::string;
-using std::map;
-using std::vector;
-using std::queue;
-using std::cin;
-using std::cout;
-using std::endl;
-
-class BorNode;
-typedef map<const char, BorNode *> LinksMap;
-
-
-class BorNode {
-public:
-    LinksMap links;
-    BorNode *fail;
-    BorNode *term;
-    int out;
-
-public:
-    BorNode(BorNode *fail_node = NULL)
-        : fail(fail_node)
-        , term(NULL)
-        , out(-1)
-    { }
-
-    BorNode* getLink(const char c) const
-    {
-        LinksMap::const_iterator iter = links.find(c);
-        if (iter != links.cend()) {
-            return iter->second;
-        }
-        else {
-            return NULL;
-        }
-    }
-
-    bool isTerminal() const
-    {
-        return (out >= 0);
-    }
-};
-
-class AhoCorasick
-{
-public:
-    typedef void (*Callback) (const char* substr);
-    BorNode root;
-    vector<string> words;
-    BorNode* current_state;
-
-public:
-    void addString(const char* const str)
-    {
-        BorNode *current_node = &root;
-        for(const char *cp = str; *cp; ++cp) {
-            BorNode *child_node = current_node->getLink(*cp);
-            if (!child_node) {
-                child_node = new BorNode(&root);
-                current_node->links[*cp] = child_node;
-            }
-            current_node = child_node;
-        }
-        current_node->out = words.size();
-        words.push_back(str);
-    }
-
-    void init()
-    {
-        queue<BorNode *> q;
-        q.push(&root);
-        while (!q.empty()) {
-            BorNode *current_node = q.front();
-            q.pop();
-            for (LinksMap::const_iterator iter = current_node->links.cbegin();
-                 iter != current_node->links.cend(); ++iter)
-            {
-                const char symbol = iter->first;
-                BorNode *child = iter->second;
-
-
-                BorNode *temp_node = current_node->fail;
-                while (temp_node) {
-                    BorNode *fail_candidate = temp_node->getLink(symbol);
-                    if (fail_candidate) {
-                        child->fail = fail_candidate;
-                        break;
-                    }
-                    temp_node = temp_node->fail;
-                }
-
-
-                    if (child->fail->isTerminal()) {
-                        child->term = child->fail;
-                    }
-                    else {
-                        child->term = child->fail->term;
-                    }
-                q.push(child);
-            }
-        }
-    }
-
-    void step(const char c)
-    {
-        while (current_state) {
-            BorNode *candidate = current_state->getLink(c);
-            if (candidate) {
-                current_state = candidate;
-                return;
-            }
-            current_state = current_state->fail;
-        }
-        current_state = &root;
-    }
-
-    void printTermsForCurrentState(Callback callback) const
-    {
-        if (current_state->isTerminal()) {
-            callback(words[current_state->out].c_str());
-        }
-        BorNode *temp_node = current_state->term;
-        while (temp_node) {
-            callback(words[temp_node->out].c_str());
-            temp_node = temp_node->term;
-        }
-    }
-
-    void search(const char* str, Callback callback)
-    {
-        current_state = &root;
-        for(; *str; ++str) {
-            cout << *str << ':' << endl;
-            step(*str);
-            printTermsForCurrentState(callback);
-         }
-    }
-};
-}
+//log all the information
 inline void log(int id, int count, std::string filename, int total){
   using nlohmann::json;
   if(count > 5 && (count / (double)total) > 0.01){
@@ -187,12 +32,6 @@ inline void log(int id, int count, std::string filename, int total){
     std::cout<<js<<std::endl;
   }
 }
-
-
-
-
-
-
 
 
 
@@ -219,6 +58,8 @@ int main(int argc, const char * argv[]) {
   return 0;
 }
 
+/* run this function on large memory machine
+*/
 void large_memory_mode(){
   std::string filename = "dump.dat";
   auto signList = util::getAllSignatures(filename);
@@ -271,6 +112,8 @@ void limited_memory_mode(){
   std::string path;
 
   int tree_count = 0;
+
+
   while(1){
     std::cout<<"Ready to start"<<std::endl;
     std::cin >> path;
@@ -278,20 +121,25 @@ void limited_memory_mode(){
     std::vector<std::tuple<std::string, std::string>> files;
     std::ifstream list_f("list");
     std::string buf;
+
     while(list_f.good() && !list_f.eof() && std::getline(list_f, buf)){
       files.push_back(std::make_tuple(buf, util::getHexString(buf)));
-    }
+    }//read every file
+
     for(auto sign:signList){
       tree_count = 0;
       int id = std::get<0>(sign);
       aho_corasick::trie trie;
+
       for(auto entry: std::get<1>(sign)){
-        if(10 < entry.size() < 10000){
+        if(10 < entry.size() && entry.size() < 10000){
           trie.insert(entry);
           ++tree_count;
         }
-      }
+      }//iterate through every signatures in a components and build a trie tree
+
       if(tree_count != 0){
+
         for(const auto& file_tu: files){
           std::string file = std::get<0>(file_tu);
           std::unordered_set<std::string> match_set;
@@ -304,9 +152,11 @@ void limited_memory_mode(){
           }
           int count = match_set.size();
           log(id, count, file, tree_count);
-        }
+
+          
+        }//end of file loop
       }
-    }
+    }//end of components loop
   }
 
 }
